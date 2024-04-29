d697acf5b7f9c9430936674ae6b2cb50facc9cd1
Leon Ribeiro <159721905+Leon-r9@users.noreply.github.com>
2024-04-29 20:27:12 -0300

    Update Principal.java

--- CAP-06/Exercicios-Resolvidos/QR01/src/br/edu/principal/Principal.java
+++ CAP-06/Exercicios-Resolvidos/QR01/src/br/edu/principal/Principal.java
@@ -3,39 +3,40 @@ import java.util.Scanner;
 public class Principal {
 
 	public static void main(String[] args) {
-		
-		        Scanner scanner = new Scanner(System.in);
-		        
-		        // Preenchendo o vetor com 9 números inteiros
-		        int[] vetor = new int[9];
-		        for (int i = 0; i < vetor.length; i++) {
-		            System.out.print("Digite o " + (i + 1) + "º número inteiro: ");
-		            vetor[i] = scanner.nextInt();
-		        }
-		        
-		        System.out.println("Números primos e suas respectivas posições:");
-		        for (int i = 0; i < vetor.length; i++) {
-		            if (ehPrimo(vetor[i])) {
-		                System.out.println("Número primo: " + vetor[i] + ", Posição: " + i);
-		            }
-		        }
-		        
-		        scanner.close();
-		    }
-		    
-		    // Método para verificar se um número é primo
-		    public static boolean ehPrimo(int numero) {
-		        if (numero <= 1) {
-		            return false;
-		        }
-		        for (int i = 2; i <= Math.sqrt(numero); i++) {
-		            if (numero % i == 0) {
-		                return false;
-		            }
-		        }
-		        return true;
-		    }
-		}
+		/*1. FaÃ§a um programa que preencha um vetor com nove nÃºmeros inteiros, calcule e mostre os nÃºmeros
+primos e suas respectivas posiÃ§Ãµes. */
+				
+Scanner scanner = new Scanner(System.in);
+				        
+int[] vetor = new int[9];
+for (int i = 0; i < vetor.length; i++) {
+ System.out.print("Digite o " + (i + 1) + "Âº nÃºmero inteiro: ");
+  vetor[i] = scanner.nextInt();
+    }
+				        
+ System.out.println("NÃºmeros primos e suas respectivas posiÃ§Ãµes:");
+for (int i = 0; i < vetor.length; i++) {
+if (ehPrimo(vetor[i])) {
+System.out.println("NÃºmero primo: " + vetor[i] + ", PosiÃ§Ã£o: " + i);
+}
+}
+				        
+scanner.close();
+}
+
+				   
+public static boolean ehPrimo(int numero) {
+if (numero <= 1) {
+return false;
+}
+for (int i = 2; i <= Math.sqrt(numero); i++) {
+if (numero % i == 0) {
+return false;
+}
+}
+return true;
+}
+}
 
 
 	
